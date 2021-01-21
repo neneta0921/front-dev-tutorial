3571e69816293c3ba62f134b1a74a2bac3ec914c
neneta0921 <65237907+neneta0921@users.noreply.github.com>
2021-01-21 20:01:38 +0900

    refactoring main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,41 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const hero = new HeroSlider('.swiper-container')
-  hero.start()
+  const main = new Main()
+})
 
-  const cb = (el, inview) => {
+class Main {
+  constructor() {
+    this.header = document.querySelector('.header')
+    this._observers = []
+    this._init()
+  }
+
+  set observers(val) {
+    this._observers.push(val)
+  }
+
+  get observers() {
+    return this._observers
+  }
+
+  _init() {
+    new MobileMenu()
+    this.hero = new HeroSlider('.swiper-container')
+    Pace.on('done', this._paceDone.bind(this))
+  }
+
+  _paceDone() {
+    this._scrollInit()
+  }
+
+  _navAnimation(el, inview) {
     if (inview) {
-      const ta = new TweenTextAnimation(el)
-      ta.animate()
+      this.header.classList.remove('triggered')
+    } else {
+      this.header.classList.add('triggered')
     }
   }
 
-  const so = new ScrollObserver('.tween-animate-title', cb)
-
-  const _inviewAnimation = (el, inview) => {
+  _inviewAnimation(el, inview) {
     if (inview) {
       el.classList.add('inview')
     } else {
@@ -19,18 +43,45 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
 
-  const so2 = new ScrollObserver('.cover-slide', _inviewAnimation)
-
-  const _navAnimation = (el, inview) => {
-    const header = document.querySelector('.header')
+  _textAnimation(el, inview) {
     if (inview) {
-      header.classList.remove('triggered')
-    } else {
-      header.classList.add('triggered')
+      const ta = new TweenTextAnimation(el)
+      ta.animate()
     }
   }
 
-  const so3 = new ScrollObserver('.nav-trigger', _navAnimation, { once: false })
+  _toggleSlideAnimation(el, inview) {
+    if (inview) {
+      this.hero.start()
+    } else {
+      this.hero.stop()
+    }
+  }
 
-  new MobileMenu()
-})
+  _destroyObservers() {
+    this.observers.forEach(ob => ob.destroy())
+  }
+
+  destroy() {
+    this._destroyObservers
+  }
+
+  _scrollInit() {
+    this.observers = new ScrollObserver(
+      '.nav-trigger',
+      this._navAnimation.bind(this),
+      { once: false }
+    )
+    this.observers = new ScrollObserver('.cover-slide', this._inviewAnimation)
+    this.observers = new ScrollObserver(
+      '.tween-animate-title',
+      this._textAnimation
+    )
+    this.observers = new ScrollObserver(
+      '.swiper-container',
+      this._toggleSlideAnimation.bind(this),
+      { once: false }
+    )
+    console.log(this.observers)
+  }
+}
