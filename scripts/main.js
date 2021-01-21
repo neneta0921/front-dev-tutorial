f484015768600743ec8616feae61797b38f221b5
neneta0921 <65237907+neneta0921@users.noreply.github.com>
2021-01-21 09:30:02 +0900

    create mobile view

--- scripts/main.js
+++ scripts/main.js
@@ -2,12 +2,22 @@ document.addEventListener('DOMContentLoaded', () => {
   const hero = new HeroSlider('.swiper-container')
   hero.start()
 
-  const cb = function (el, isIntersecting) {
-    if (isIntersecting) {
+  const cb = (el, inview) => {
+    if (inview) {
       const ta = new TweenTextAnimation(el)
       ta.animate()
     }
   }
 
   const so = new ScrollObserver('.tween-animate-title', cb)
+
+  const _inviewAnimation = (el, inview) => {
+    if (inview) {
+      el.classList.add('inview')
+    } else {
+      el.classList.remove('inview')
+    }
+  }
+
+  const so2 = new ScrollObserver('.cover-slide', _inviewAnimation)
 })
